25b52e1c7c5296f87c11d52a13222dfd22dbc254
Novitasyafitri <novita.redmi7a2020@gmail.com>
2025-09-25 00:15:19 +0700

    Revert "initial commit"
    This reverts commit 45bac62bd7f1f8e957e0cefc5adc4c46107f78e1.

--- src/elvisOperator.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>) {
-//Elvis Operator ?:
-    var maybeWelcome: String? = "Hello world"
-
-    println(maybeWelcome?.length ?: 0)
-}
--- src/null_safety.kt
+++
@@ -1,6 +0,0 @@
-fun main(args: Array<String>){
-//Nullable types
-    var maybeNumber: Int? = 15
-    maybeNumber = null
-    println("maybeNumber: $maybeNumber ${maybeNumber?.hashCode()}")
-}
